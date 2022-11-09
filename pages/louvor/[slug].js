df5d6b84fcc5c920c048d312ccf1da4210ed5d07
emidde <75273014+emidde@users.noreply.github.com>
2022-11-08 21:28:31 -0300

    Update [slug].js

--- pages/louvor/[slug].js
+++ pages/louvor/[slug].js
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
 const hygraph = new GraphQLClient(
-  'https://api-sa-east-1.hygraph.com/v2/cla7q578i1eud01uq4weybdlt/master'
+  ''
 );
 
 export async function getStaticProps({ params }) {
