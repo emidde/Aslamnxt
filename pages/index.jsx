3a65d67891e89857939027e7ddf490e85aeaa4ff
emidde <75273014+emidde@users.noreply.github.com>
2022-11-08 21:32:14 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -67,7 +67,7 @@ export default function Home({aslams}) {
 export async function getStaticProps() {
 
   const hygraph = new GraphQLClient(
-    'https://api-sa-east-1.hygraph.com/v2/cla7q578i1eud01uq4weybdlt/master'
+    ''
   );
 
 
